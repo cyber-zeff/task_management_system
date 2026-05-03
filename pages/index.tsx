8c3a8a8a4099e128a3efc10b5bfa318656e55325
cyber-zeff <huzaifashamsi819@gmail.com>
2026-05-03 16:38:58 +0500

    fixed the deisgn for the head

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Space } from '@prisma/client';
 import Spaces from 'components/Spaces';
 import WithNavBar from 'components/WithNavBar';
 import type { GetServerSideProps, NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import { getEnhancedPrisma } from 'server/enhanced-db';
 
@@ -20,10 +21,11 @@ const Home: NextPage<Props> = ({ spaces }) => {
                     <h1 className="text-2xl text-gray-800">Welcome {user.name || user.email}!</h1>
 
                     <div className="w-full p-8">
-                        <h2 className="text-lg md:text-xl text-left mb-8 text-gray-700">
-                            Choose a space to start, or{' '}
-                            <Link href="/create-space" className="link link-primary">
-                                create a new one.
+                        <h2 className="text-lg md:text-xl text-left mb-8 text-gray-700 flex justify-between">
+                            <h2 className='font-medium text-[24px]'>Space</h2>
+                            <Link href="/create-space" className="flex items-center gap-2 text-black">
+                                <Image src="/add.svg" alt="Arrow icon" width={20} height={20} className='mt-0.5' />
+                                Create
                             </Link>
                         </h2>
                         <Spaces spaces={spaces} />
