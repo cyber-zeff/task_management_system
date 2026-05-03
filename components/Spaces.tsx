04e8a1d4f21af0cf54f0428fe27fae08836a360d
cyber-zeff <huzaifashamsi819@gmail.com>
2026-05-03 15:15:10 +0500

    upd: finalized list design with collaborators

--- components/Spaces.tsx
+++ components/Spaces.tsx
@@ -1,23 +1,77 @@
 import { useCountList } from '@lib/hooks';
 import { Space } from '@prisma/client';
 import Link from 'next/link';
+import Image from 'next/image';
+import { useFindManySpaceUser } from '@lib/hooks';
+import Avatar from './Avatar';
 
 type Props = {
     spaces: Space[];
 };
 
-function SpaceItem({ space }: { space: Space }) {
+function SpaceItem({ space, index }: { space: Space; index: number }) {
+    const { data: members } = useFindManySpaceUser(
+        {
+            where: {
+                spaceId: space?.id,
+            },
+            include: {
+                user: true,
+            },
+            orderBy: {
+                role: 'desc',
+            },
+        },
+        { disabled: !space }
+    );
+
     const { data: listCount } = useCountList({
         where: { spaceId: space.id },
     });
+    const colors = [
+        "bg-[#F2995E]", // orange
+        "bg-[#7FB2FF]", // blue
+        "bg-[#AFA3FF]"  // purple
+    ];
+
+    const hoverColors = [
+        "hover:bg-[#e68a4f]",
+        "hover:bg-[#6aa3f5]",
+        "hover:bg-[#9b8df5]"
+    ];
+
+    const color = colors[index % colors.length];
+    const hover = hoverColors[index % hoverColors.length];
+
     return (
-        <div className="w-full h-full flex relative justify-center items-center bg-[#EC683E] hover:bg-[#e6653e] text-[#fff] rounded-[25px]">
-            <div className="badge badge-outline badge-sm absolute top-4 right-4">{listCount}</div>
-            <Link href={`/space/${space.slug}`}>
-                <div className="card-body" title={`${space.name} ${listCount ? ': ' + listCount + ' lists' : ''}`}>
-                    <h2 className="card-title line-clamp-1">{space.name}</h2>
+        <div className={`w-full h-full flex flex-col justify-between relative ${color} ${hover} text-black rounded-[25px] p-4`}>
+
+            <Link href={`/space/${space.slug}`} className='flex justify-between items-center '>
+                <div
+                    className="card-body p-0"
+                    title={`${space.name} ${listCount ? ': ' + listCount + ' lists' : ''}`}
+                >
+                    <h2 className="card-title line-clamp-0 text-[20px] w-[70%] text-left">{space.name}</h2>
+                </div>
+                <div>
+                    <Image src="/arrow.svg" alt="arrow-icon" width={25} height={25} className='mr-4' />
                 </div>
             </Link>
+
+            <div className="flex justify-between">
+                <div>
+                    {listCount} {listCount == 1 ? "List" : "Lists"}
+                </div>
+                <div>
+                    {members && (
+                        <label className="mr-1 modal-button cursor-pointer" htmlFor="management-modal">
+                            {members?.map((member) => (
+                                <Avatar key={member.id} user={member.user} size={24} />
+                            ))}
+                        </label>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
@@ -25,12 +79,12 @@ function SpaceItem({ space }: { space: Space }) {
 export default function Spaces({ spaces }: Props) {
     return (
         <ul className="flex flex-wrap gap-4">
-            {spaces?.map((space) => (
+            {spaces?.map((space, index) => (
                 <li
-                    className="card w-80 h-32 flex justify-center shadow-xl cursor-pointer bg-[#EC683E]"
+                    className="card w-80 h-32 flex justify-start shadow-xl cursor-pointer rounded-[25px] border border-black"
                     key={space.id}
                 >
-                    <SpaceItem space={space} />
+                    <SpaceItem space={space} index={index} />
                 </li>
             ))}
         </ul>
